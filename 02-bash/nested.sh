01a3164c489b2b2b47fb826a1db297c0d9e43eba
samhyderrr <shaheryar.h1@gmail.com>
2026-07-28 00:16:49 +0100

    updated the notes on the script

--- 02-bash/nested.sh
+++ 02-bash/nested.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# variables to store student information
+age=18     # variable 1
+grade=80     # variable 2
 
-age=18     # store the students age
-grade=80     # store the students grade
+# first if statement to check if the student is eligible based on age
 
-#first if statement to check if the student is eligible based on age
-
-if [ $age -gt 18 ]; then    # check if age is greater than 18
+if [ $age -gt 18 ]; then                                                    # check if age is greater than 18
     echo "You are eligible based on age."
 
     #nested if statement and will only run if the age is greater than 18
 
-    if [ $grade -ge 80 ]; then      # is grade greater than or equal to 80
+    if [ $grade -ge 80 ]; then                                              # is grade greater than or equal to 80
         echo "You are also eligible based on grade."
         echo "Congratulations!"
-    else    # grade is less than 80
+    else                                                                    # grade is less than 80
         echo "However, your grade is below the required threshold."
-    fi  #end nested if statement
-else    #age was NOT greater than 18
+    fi                                                                      #end nested if statement
+else                                                                        #age was NOT greater than 18
     echo "You are not eligible based on age."   
-fi      # end first if statement
+fi                                                                          # end first if statement
